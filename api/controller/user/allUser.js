4641c4019b97b1cd6d2bd125d44d4934542ef477
ZIHAD22 <mdzihad2244@gmail.com>
2022-08-05 01:51:29 +0600

    add search function

--- api/controller/user/allUser.js
+++ api/controller/user/allUser.js
@@ -1,7 +1,8 @@
 const User = require("../../models/User");
 
 const allUser = async (req, res) => {
-  const { email: loginUserEmail } = req.query;
+  const { email: loginUserEmail, searchKey } = req.query;
+  console.log(searchKey);
 
   const result = await User.find({}).sort({ _id: -1 });
 
@@ -9,7 +10,15 @@ const allUser = async (req, res) => {
     return res.json({ success: false, result: "Not Found" });
   }
 
-  const filteredUser = result.filter((user) => user.email !== loginUserEmail);
+  let filteredUser = result.filter((user) => user.email !== loginUserEmail);
+
+  if (searchKey) {
+    const searchResult = filteredUser.filter((user) =>
+      user.name.toLowerCase().includes(searchKey)
+    );
+
+    return res.json({ success: true, result: searchResult });
+  }
 
   res.json({ success: true, result: filteredUser });
 };
