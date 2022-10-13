36477edc897778e41ce9e2515cc71659885af32d
ZIHAD22 <mdzihad2244@gmail.com>
2022-10-14 00:31:52 +0600

    add socket server

---
+++ socket-server/socketServer.js
@@ -0,0 +1,47 @@
+const io = require("socket.io")(7000, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+})
+
+let users = []
+
+const addUser = (userId, socketId) => {
+    !users.some((user) => user.userId === userId) && users.push({ userId, socketId })
+}
+
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+    return users.find((user) => user.userId === userId);
+};
+
+
+io.on("connection", (socket) => {
+    console.log("socket server conn");
+    // when connected
+    socket.on("addUser", (authUserId) => {
+        addUser(authUserId, socket.id)
+        socket.emit("getUsers", users)
+    })
+
+    // messages
+    socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        io.to(user.socketId).emit("getMessage", {
+            sender: senderId,
+            message: text,
+        });
+    })
+
+
+    // when disconnect
+    socket.on("disconnect", () => {
+        console.log("a user disconnect");
+        removeUser(socket.id)
+        io.emit("getUsers", users)
+    })
+
+})
